9f4a2ee7034aa4779790566818f1262603a68390
kaina <jianhui.xu@aihuishou.com>
2019-02-10 20:26:26 +0800

    动态按钮

--- lib/pages/detail/look_confirm_button.dart
+++ lib/pages/detail/look_confirm_button.dart
@@ -3,35 +3,37 @@ import 'package:flutter/cupertino.dart';
 
 typedef VoidCallback = void Function();
 
-Color _defaultColor;
-
 ///想看、看过的按钮
 class LookConfirmButton extends StatefulWidget {
   final String btnText;
   final String iconAsset;
-
   final Color pressedColor;
   final VoidCallback onPressed;
+  final Color defaultColor;
 
   LookConfirmButton(
       {Key key,
       @required this.btnText,
       @required this.iconAsset,
-      @required defaultColor,
       @required this.pressedColor,
+      @required this.defaultColor,
       this.onPressed})
-      : super(key: key) {
-    _defaultColor = defaultColor;
-  }
+      : super(key: key);
 
   @override
   State<StatefulWidget> createState() {
-    return _State();
+    return _State(defaultColor);
   }
 }
 
 class _State extends State<LookConfirmButton> {
-  var color = _defaultColor;
+  var _color;
+  Color _defaultColor;
+
+  _State(Color color) {
+    _color = color;
+    _defaultColor = color;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -40,7 +42,8 @@ class _State extends State<LookConfirmButton> {
         alignment: Alignment.center,
         height: 44.0,
         decoration: BoxDecoration(
-            color: color, borderRadius: BorderRadius.all(Radius.circular(5.0))),
+            color: _color,
+            borderRadius: BorderRadius.all(Radius.circular(5.0))),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
@@ -66,12 +69,12 @@ class _State extends State<LookConfirmButton> {
       },
       onTapDown: (TapDownDetails details) {
         setState(() {
-          color = widget.pressedColor;
+          _color = widget.pressedColor;
         });
       },
       onTapUp: (TapUpDetails details) {
         setState(() {
-          color = _defaultColor;
+          _color = _defaultColor;
         });
       },
     );
