4a315e6eca947b066e8a3781dc5fd7dcc4b3148d
kaina <jianhui.xu@aihuishou.com>
2019-02-11 11:23:31 +0800

    添加video_player依赖

---
+++ lib/widgets/video_widget.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+
+class VideoWidget extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _VideoWidgetState();
+  }
+}
+
+class _VideoWidgetState extends State<VideoWidget> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+
+}
