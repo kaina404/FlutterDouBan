d304ae5b71eff63afc33ee993c205ad6f3e02d41
kaina <jianhui.xu@aihuishou.com>
2019-02-11 23:39:16 +0800

    视频播放

--- lib/http/API.dart
+++ lib/http/API.dart
@@ -36,7 +36,8 @@ class API {
         resultList.map<MovieBean>((item) => MovieBean.fromMap(item)).toList();
     requestCallBack(list);
   }
-///https://api.douban.com/v2/movie/in_theaters?apikey=0b2bdeda43b5688921839c8ecb20399b
+
+  ///https://api.douban.com/v2/movie/in_theaters?apikey=0b2bdeda43b5688921839c8ecb20399b
   void getIntheaters(RequestCallBack requestCallBack) async {
     final Map result = await _request.get(IN_THEATERS);
     var resultList = result['subjects'];
@@ -83,5 +84,12 @@ class API {
         '/v2/movie/subject/$subjectId?apikey=0b2bdeda43b5688921839c8ecb20399b');
     MovieDetailBean bean = MovieDetailBean.fromJson(result);
     requestCallBack(bean);
+  }
+
+  ///电影短评
+  ///https://api.douban.com/v2/movie/subject/26266893/comments?apikey=0b2bdeda43b5688921839c8ecb20399b
+  void getComents(subjectId, RequestCallBack requestCallBack) async {
+    final result = await _request.get(
+        '/v2/movie/subject/$subjectId/comments?apikey=0b2bdeda43b5688921839c8ecb20399b');
   }
 }
