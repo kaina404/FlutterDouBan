b0f187bf945644e0b187f9065a9e8efb35da1027
kaina <jianhui.xu@aihuishou.com>
2019-02-13 22:40:57 +0800

    动态放大图片

--- lib/pages/photo_hero_page.dart
+++ lib/pages/photo_hero_page.dart
@@ -11,16 +11,15 @@ class PhotoHeroPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        alignment: Alignment.center,
-        child: _PhotoHero(
-          photoUrl: photoUrl,
-          width: width,
-          onTap: () {
-            Navigator.of(context).pop();
-          },
-        ),
+    return Container(
+      color: Colors.transparent,
+      alignment: Alignment.center,
+      child: _PhotoHero(
+        photoUrl: photoUrl,
+        width: width,
+        onTap: () {
+          Navigator.of(context).pop();
+        },
       ),
     );
   }
@@ -52,56 +51,56 @@ class _PhotoHero extends StatelessWidget {
     );
   }
 }
-
-class HeroAnimation extends StatelessWidget {
-  final String url;
-
-  HeroAnimation(this.url, {Key key}) : super(key: key);
-
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Basic Hero Animation'),
-      ),
-      body: Center(
-        child: PhotoHero(
-            photo: url,
-            width: 300.0,
-            onTap: () {
-              Router.push(
-                  context, Router.photoHero, {'photoUrl': url, 'width': 100.0});
-            }),
-      ),
-    );
-  }
-}
-
-class PhotoHero extends StatelessWidget {
-  const PhotoHero({Key key, this.photo, this.onTap, this.width})
-      : super(key: key);
-
-  final String photo;
-  final VoidCallback onTap;
-  final double width;
-
-  Widget build(BuildContext context) {
-    return SizedBox(
-      width: width,
-      child: Hero(
-        tag: photo,
-        child: Material(
-          color: Colors.transparent,
-          child: InkWell(
-            onTap: onTap,
-            child: ListView.builder(itemBuilder: (BuildContext context, int index){
-              return Image.network(
-                photo,
-                fit: BoxFit.contain,
-              );
-            }, itemCount: 20,),
-          ),
-        ),
-      ),
-    );
-  }
-}
+//
+//class HeroAnimation extends StatelessWidget {
+//  final String url;
+//
+//  HeroAnimation(this.url, {Key key}) : super(key: key);
+//
+//  Widget build(BuildContext context) {
+//    return Scaffold(
+//      appBar: AppBar(
+//        title: const Text('Basic Hero Animation'),
+//      ),
+//      body: Center(
+//        child: PhotoHero(
+//            photo: url,
+//            width: 300.0,
+//            onTap: () {
+//              Router.push(
+//                  context, Router.photoHero, {'photoUrl': url, 'width': 100.0});
+//            }),
+//      ),
+//    );
+//  }
+//}
+//
+//class PhotoHero extends StatelessWidget {
+//  const PhotoHero({Key key, this.photo, this.onTap, this.width})
+//      : super(key: key);
+//
+//  final String photo;
+//  final VoidCallback onTap;
+//  final double width;
+//
+//  Widget build(BuildContext context) {
+//    return SizedBox(
+//      width: width,
+//      child: Hero(
+//        tag: photo,
+//        child: Material(
+//          color: Colors.transparent,
+//          child: InkWell(
+//            onTap: onTap,
+//            child: ListView.builder(itemBuilder: (BuildContext context, int index){
+//              return Image.network(
+//                photo,
+//                fit: BoxFit.contain,
+//              );
+//            }, itemCount: 20,),
+//          ),
+//        ),
+//      ),
+//    );
+//  }
+//}
