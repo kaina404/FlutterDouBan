f822d1bf35068b85acee86ff3c9d7062b9e9aee4
kaina <lovexujh@126.com>
2019-03-11 12:03:38 +0800

    注释

--- lib/pages/container_page.dart
+++ lib/pages/container_page.dart
@@ -14,10 +14,10 @@ class ContainerPage extends StatefulWidget {
   }
 }
 
-class Item {
+class _Item {
   String name, activeIcon, normalIcon;
 
-  Item(this.name, this.activeIcon, this.normalIcon);
+  _Item(this.name, this.activeIcon, this.normalIcon);
 }
 
 class _ContainerPageState extends State<ContainerPage> {
@@ -27,15 +27,15 @@ class _ContainerPageState extends State<ContainerPage> {
   final defaultItemColor = Color.fromARGB(255, 125, 125, 125);
 
   final itemNames = [
-    Item('首页', 'assets/images/ic_tab_home_active.png',
+    _Item('首页', 'assets/images/ic_tab_home_active.png',
         'assets/images/ic_tab_home_normal.png'),
-    Item('书影音', 'assets/images/ic_tab_subject_active.png',
+    _Item('书影音', 'assets/images/ic_tab_subject_active.png',
         'assets/images/ic_tab_subject_normal.png'),
-    Item('小组', 'assets/images/ic_tab_group_active.png',
+    _Item('小组', 'assets/images/ic_tab_group_active.png',
         'assets/images/ic_tab_group_normal.png'),
-    Item('市集', 'assets/images/ic_tab_shiji_active.png',
+    _Item('市集', 'assets/images/ic_tab_shiji_active.png',
         'assets/images/ic_tab_shiji_normal.png'),
-    Item('我的', 'assets/images/ic_tab_profile_active.png',
+    _Item('我的', 'assets/images/ic_tab_profile_active.png',
         'assets/images/ic_tab_profile_normal.png')
   ];
 
@@ -77,7 +77,7 @@ class _ContainerPageState extends State<ContainerPage> {
   int _selectIndex = 0;
 
 //Stack（层叠布局）+Offstage组合,解决状态被重置的问题
-  Widget getWidget(int index) {
+  Widget _getPagesWidget(int index) {
     return Offstage(
       offstage: _selectIndex != index,
       child: TickerMode(
@@ -116,11 +116,11 @@ class _ContainerPageState extends State<ContainerPage> {
     return Scaffold(
       body: new Stack(
         children: [
-          getWidget(0),
-          getWidget(1),
-          getWidget(2),
-          getWidget(3),
-          getWidget(4),
+          _getPagesWidget(0),
+          _getPagesWidget(1),
+          _getPagesWidget(2),
+          _getPagesWidget(3),
+          _getPagesWidget(4),
         ],
       ),
 //        List<BottomNavigationBarItem>
@@ -134,6 +134,7 @@ class _ContainerPageState extends State<ContainerPage> {
         onTap: (int index) {
           setState(() {
             _selectIndex = index;
+            //这个是用来控制比较特别的shopPage中WebView不能动态隐藏的问题
             shopPageWidget.setShowState(pages.indexOf(shopPageWidget) == _selectIndex);
           });
         },
