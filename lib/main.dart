944133005db3b7e60b153944f4c46bafa6efd0fa
kaina <lovexujh@126.com>
2019-03-12 17:00:09 +0800

    删除冗余代码

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:douban_app/pages/container_page.dart';
-import 'package:douban_app/pages/movie/HotSoonMovieWidget.dart';
-import 'package:douban_app/http/API.dart';
-import 'package:douban_app/pages/movie/TitleWidget.dart';
-import 'package:douban_app/pages/movie/TodayPlayMovieWidget.dart';
-import 'package:douban_app/widgets/subject_mark_image_widget.dart';
-import 'package:douban_app/bean/subject_entity.dart';
 import 'dart:io';
 import 'package:flutter/services.dart';
-import 'package:douban_app/pages/home/my_home_tab_bar.dart';
-import 'package:douban_app/demo/ImageColors.dart';
-import 'package:douban_app/widgets/video_widget.dart';
-import 'package:douban_app/widgets/video_progress_bar.dart';
-import 'package:douban_app/http/API.dart';
-import 'package:douban_app/widgets/title_bar.dart';
-import 'package:douban_app/pages/photo_hero_page.dart';
 import 'package:douban_app/widgets/bottom_drag_widget.dart';
-import 'package:douban_app/pages/detail/long_comment_widget.dart';
-import 'package:douban_app/widgets/search_text_field_widget.dart';
-import 'dart:math' as math;
-import 'package:douban_app/pages/home/home_app_bar.dart' as myapp;
 
 void main() {
   runApp(MyApp());
@@ -44,64 +27,6 @@ class MyApp extends StatelessWidget {
         resizeToAvoidBottomPadding: false,
         body: ContainerPage(),
       ),
-    );
-  }
-
-  Widget _buildButtonItem(IconData icon, String text) {
-    return Expanded(
-        child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Icon(icon, size: 28.0),
-        Container(
-          margin: EdgeInsets.only(top: 8.0),
-          child: Text(text,
-              style: TextStyle(color: Color(0xff999999), fontSize: 12)),
-        )
-      ],
-    ));
-  }
-
-  Widget _buildTabBarBg() {
-    return Container(
-      //TabBar圆角背景颜色
-      height: 50,
-      child: ClipRRect(
-          borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(15), topRight: Radius.circular(15)),
-          child: Container(color: Colors.white)),
-    );
-  }
-}
-
-class Demo extends StatefulWidget {
-  @override
-  _DemoState2 createState() => _DemoState2();
-}
-
-class _DemoState2 extends State<Demo> {
-  @override
-  Widget build(BuildContext context) {
-    return CustomScrollView(
-      slivers: <Widget>[
-        myapp.SliverAppBar(
-          title: SearchTextFieldWidget(
-            hintText: '搜索',
-          ),
-          floating: true,
-          pinned: false,
-          flexibleSpace: FlexibleSpaceBar(
-            background: FlutterLogo(),
-            title: Text('费话费'),
-          ),
-          expandedHeight: 200.0,
-        ),
-        SliverList(
-            delegate:
-                SliverChildBuilderDelegate((BuildContext context, int index) {
-          return Text("jfiewo$index");
-        }, childCount: 100))
-      ],
     );
   }
 }
