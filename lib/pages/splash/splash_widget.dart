2a0dc63c41119adcaa317281403d1ffc4cd2c5de
kaina <lovexujh@126.com>
2019-03-15 10:10:05 +0800

    优化广告页面

--- lib/pages/splash/splash_widget.dart
+++ lib/pages/splash/splash_widget.dart
@@ -11,41 +11,18 @@ class SplashWidget extends StatefulWidget {
 }
 
 class _SplashWidgetState extends State<SplashWidget> {
-  Timer _timer;
   var container = ContainerPage();
 
-  var _seconds = 6;
-
-  @override
-  void initState() {
-    super.initState();
-    _startTimer();
-  }
-
-  /// 启动倒计时的计时器。
-  void _startTimer() {
-    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
-      setState(() {});
-      if (_seconds == 0) {
-        _cancelTimer();
-        return;
-      }
-      _seconds--;
-    });
-  }
-
-  /// 取消倒计时的计时器。
-  void _cancelTimer() {
-    _timer?.cancel();
-  }
+  bool showAd = true;
 
   @override
   Widget build(BuildContext context) {
+    print('build splash');
     return Stack(
       children: <Widget>[
         Offstage(
           child: container,
-          offstage: _seconds > 0,
+          offstage: showAd,
         ),
         Offstage(
           child: Container(
@@ -58,63 +35,114 @@ class _SplashWidgetState extends State<SplashWidget> {
                     radius: ScreenUtils.screenW() / 3,
                     backgroundColor: Colors.white,
                     backgroundImage:
-                    AssetImage(Constant.ASSETS_IMG + 'home.png'),
+                        AssetImage(Constant.ASSETS_IMG + 'home.png'),
                   ),
                 ),
                 SafeArea(
                     child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Align(
-                          alignment: Alignment(1.0, 0.0),
-                          child: Container(
-                            margin: const EdgeInsets.only(
-                                right: 30.0, top: 20.0),
-                            padding: const EdgeInsets.only(
-                                left: 10.0, right: 10.0, top: 2.0, bottom: 2.0),
-                            child: Text(
-                              '$_seconds',
-                              style: TextStyle(fontSize: 17.0),
-                            ),
-                            decoration: BoxDecoration(
-                                color: Color(0xffEDEDED),
-                                borderRadius:
-                                const BorderRadius.all(Radius.circular(10.0))),
-                          ),
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Align(
+                      alignment: Alignment(1.0, 0.0),
+                      child: Container(
+                        margin: const EdgeInsets.only(right: 30.0, top: 20.0),
+                        padding: const EdgeInsets.only(
+                            left: 10.0, right: 10.0, top: 2.0, bottom: 2.0),
+                        child: CountDownWidget(
+                          onCountDownFinishCallBack: (bool value) {
+                            if (value) {
+                              setState(() {
+                                showAd = false;
+                              });
+                            }
+                          },
                         ),
-                        Padding(
-                          padding: const EdgeInsets.only(bottom: 40.0),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              Image.asset(
-                                Constant.ASSETS_IMG + 'ic_launcher.png',
-                                width: 50.0,
-                                height: 50.0,
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.only(left: 10.0),
-                                child: Text(
-                                  'Hi,豆芽',
-                                  style: TextStyle(
-                                      color: Colors.green,
-                                      fontSize: 30.0,
-                                      fontWeight: FontWeight.bold),
-                                ),
-                              )
-                            ],
+                        decoration: BoxDecoration(
+                            color: Color(0xffEDEDED),
+                            borderRadius:
+                                const BorderRadius.all(Radius.circular(10.0))),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 40.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Image.asset(
+                            Constant.ASSETS_IMG + 'ic_launcher.png',
+                            width: 50.0,
+                            height: 50.0,
                           ),
-                        )
-                      ],
-                    ))
+                          Padding(
+                            padding: const EdgeInsets.only(left: 10.0),
+                            child: Text(
+                              'Hi,豆芽',
+                              style: TextStyle(
+                                  color: Colors.green,
+                                  fontSize: 30.0,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ))
               ],
             ),
             width: ScreenUtils.screenW(),
             height: ScreenUtils.screenH(),
           ),
-          offstage: _seconds <= 0,
+          offstage: !showAd,
         )
       ],
     );
+  }
+}
+
+class CountDownWidget extends StatefulWidget {
+  final onCountDownFinishCallBack;
+
+  CountDownWidget({Key key, @required this.onCountDownFinishCallBack})
+      : super(key: key);
+
+  @override
+  _CountDownWidgetState createState() => _CountDownWidgetState();
+}
+
+class _CountDownWidgetState extends State<CountDownWidget> {
+  var _seconds = 6;
+  Timer _timer;
+
+  @override
+  void initState() {
+    super.initState();
+    _startTimer();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      '$_seconds',
+      style: TextStyle(fontSize: 17.0),
+    );
+  }
+
+  /// 启动倒计时的计时器。
+  void _startTimer() {
+    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      setState(() {});
+      if (_seconds <= 1) {
+        widget.onCountDownFinishCallBack(true);
+        _cancelTimer();
+        return;
+      }
+      _seconds--;
+    });
+  }
+
+  /// 取消倒计时的计时器。
+  void _cancelTimer() {
+    _timer?.cancel();
   }
 }
