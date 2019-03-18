0f1207f9b2144dc8df65482e338f5470bb12af87
kaina <lovexujh@126.com>
2019-03-18 17:19:42 +0800

    重启APP

--- lib/main.dart
+++ lib/main.dart
@@ -32,6 +32,7 @@ class MyApp extends StatelessWidget {
 }
 
 ///这个组件用来重新加载整个child Widget的。当我们需要重启APP的时候，可以使用这个方案
+///https://stackoverflow.com/questions/50115311/flutter-how-to-force-an-application-restart-in-production-mode
 class RestartWidget extends StatefulWidget {
   final Widget child;
 
